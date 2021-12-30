cdd8f7a67843a7dbec00e170dbe4dbaaa3e44ee0
Raja Beemi <beemi.raja@gmail.com>
2021-12-30 23:59:14 +0000

    feat: added some docs

--- src/test/java/com/meekventures/android/SimpleTest.java
+++ src/test/java/com/meekventures/android/SimpleTest.java
@@ -3,9 +3,11 @@ package com.meekventures.android;
 import io.appium.java_client.AppiumDriver;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
@@ -15,16 +17,17 @@ import java.net.URL;
 public class SimpleTest {
 
     private static WebDriver driver;
+    private String emailAddress = "rajmentor-KattieSchuppeGladyce.Lind99@gmail.com";
+    private String password = "test123";
 
     @BeforeAll
     static void setUp() {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("deviceName", "Android Emulator");
+        desiredCapabilities.setCapability("deviceName", "Pixel XL API 30");
         desiredCapabilities.setCapability("automationName", "UiAutomator2");
-        desiredCapabilities.setCapability("app", "/Users/meekventures/Downloads/app-debug.apk");
-        desiredCapabilities.setCapability("appPackage", "com.meekventures.android");
-        desiredCapabilities.setCapability("appActivity", "com.meekventures.android.MainActivity");
+        desiredCapabilities.setCapability("app", "/Users/R.Beemireddy/Documents/projects/personal/meek-android-appium-tests/src/test/resources/apps/beta/meek-android-debug.apk");
+        desiredCapabilities.setCapability("appPackage", "com.meekventures.meek");
 
         try {
             driver = new AppiumDriver(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
@@ -44,8 +47,20 @@ public class SimpleTest {
 
 
     @Test
+    @Disabled
     public void test() {
-        driver.switchTo().defaultContent();
-        driver.findElement(By.name("test")).click();
+        WebElement email = driver.findElement(By.id("com.meekventures.meek:id/edtEmail"));
+        email.click();
+        email.sendKeys(this.emailAddress);
+
+        WebElement password = driver.findElement(By.id("com.meekventures.meek:id/edtPassword"));
+        password.click();
+        password.sendKeys(this.password);
+
+        WebElement keepMeLoggedIn = driver.findElement(By.id("com.meekventures.meek:id/checkBoxLogin"));
+        keepMeLoggedIn.click();
+
+        WebElement loginButton = driver.findElement(By.partialLinkText("Login"));
+        loginButton.click();
     }
 }
