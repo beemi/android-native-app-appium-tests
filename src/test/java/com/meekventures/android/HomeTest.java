51e742be1d95fd593f4b99a191bfb87acb76cf34
Raja Beemi <beemi.raja@gmail.com>
2021-12-31 13:35:45 +0000

    feat: Student registration done

--- src/test/java/com/meekventures/android/HomeTest.java
+++ src/test/java/com/meekventures/android/HomeTest.java
@@ -1,8 +1,10 @@
 package com.meekventures.android;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidElement;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -10,8 +12,10 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class HomeTest {
@@ -46,7 +50,8 @@ public class HomeTest {
     }
 
     @Test
-    public void test() throws InterruptedException {
+    @Disabled
+    public void home_screen_tests() throws InterruptedException {
         WebElement email = driver.findElement(By.id("edtEmail"));
         assertThat(email.isDisplayed()).as("email input should be displayed").isTrue();
         email.sendKeys(emailAddress);
@@ -90,5 +95,62 @@ public class HomeTest {
         menuSocieties.click();
         driver.findElement(By.id("imgAddSociety")).isDisplayed();
         driver.findElement(By.id("imgBack")).click();
+    }
+    @Test
+    public void student_register() throws InterruptedException {
+        WebElement email = driver.findElement(By.id("edtEmail"));
+        assertThat(email.isDisplayed()).as("email input should be displayed").isTrue();
+        WebElement joinNow = driver.findElement(By.xpath("//*[@text='Join Now!']"));
+        joinNow.click();
+
+        WebElement studentRegisterOption = driver.findElement(By.id("sign_up_as_student_image"));
+        assertThat(studentRegisterOption.isDisplayed()).as("Signup as student option displayed").isTrue();
+        studentRegisterOption.click();
+
+        WebElement firstName = driver.findElement(By.id("edtFirstName"));
+        assertThat(firstName.isDisplayed()).as("First name input should be displayed").isTrue();
+        firstName.sendKeys("Raju");
+        driver.hideKeyboard();
+
+        WebElement lastName = driver.findElement(By.id("edtLastName"));
+        assertThat(lastName.isDisplayed()).as("Last name input should be displayed").isTrue();
+        lastName.sendKeys("Beemireddy");
+        driver.hideKeyboard();
+
+        WebElement emailAddress = driver.findElement(By.id("edtEmail"));
+        assertThat(emailAddress.isDisplayed()).as("Email input should be displayed").isTrue();
+        emailAddress.sendKeys("test-" + (UUID.randomUUID().toString().replaceAll("-","")) + "@example.com");
+        driver.hideKeyboard();
+
+        WebElement password = driver.findElement(By.id("edtPassword"));
+        assertThat(password.isDisplayed()).as("Password input should be displayed").isTrue();
+        password.sendKeys("test1212121");
+        driver.hideKeyboard();
+
+        WebElement confirmPassword = driver.findElement(By.id("edtConfirmPassword"));
+        assertThat(confirmPassword.isDisplayed()).as("Confirm password input should be displayed").isTrue();
+        confirmPassword.sendKeys("test1212121");
+        driver.hideKeyboard();
+
+        WebElement selectUniversity = driver.findElement(By.id("universitySpinner"));
+        assertThat(selectUniversity.isDisplayed()).as("University input should be displayed").isTrue();
+        selectUniversity.click();
+        WebElement myUniversity = driver.findElement(By.id("search_src_text"));
+        myUniversity.sendKeys("Abertay University");
+        List universityList = driver.findElementsById("tv_search_list_item");
+        assertThat(universityList.size()).as("University list should be displayed").isGreaterThan(0);
+        ((AndroidElement) universityList.get(0)).click();
+
+        WebElement ternsAndCondition = driver.findElement(By.id("termsCondition"));
+        assertThat(ternsAndCondition.isDisplayed()).as("Terms and condition input should be displayed").isTrue();
+        ternsAndCondition.click();
+
+        WebElement signupButton = driver.findElement(By.id("signUpNowButton"));
+        assertThat(signupButton.isDisplayed()).as("Signup button should be displayed").isTrue();
+        signupButton.click();
+
+        WebElement registrationSuccess = driver.findElementById("confirmEmailHint");
+        assertThat(registrationSuccess.isDisplayed()).as("Registration success message should be displayed").isTrue();
+        assertThat(driver.findElementById("loginBtn").isDisplayed()).as("Login Now displayed").isTrue();
     }
 }
